37d7ff24bf860e8d4ac88fe95985669cea4d9a2a
Arisha Arif <arishaarif47@gmail.com>
2026-07-20 14:39:35 +0500

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.87%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-         int l = 0;
- unordered_map<int, int>hash;
- auto h = max_element(nums.begin(), nums.end());
- //we can just use array size since the seq always starts from 0
- for (auto& i : nums) {
-     hash[i]++;
- }
- while (l < *h) {
-     if (hash.find(l) == hash.end()) {
-         return l;
-     }
-     l++;
- }
- return *h + 1;
+         long long size = nums.size();
+long long expectedsum = 0;
+long long actualsum = 0;
+for (int i = 0; i <= size; i++) {
+    expectedsum += i;
+}
+for (auto& i : nums) {
+    actualsum += i;
+}
+return expectedsum - actualsum;
     }
 };
