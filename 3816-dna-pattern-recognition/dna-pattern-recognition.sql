ef5967ff18f2c5a5dcc89676b7824a783a799df8
Arisha Arif <arishaarif47@gmail.com>
2026-09-11 17:44:46 +0500

    Time: 305 ms (69.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select sample_id,dna_sequence,species,dna_sequence like 'ATG%' as has_start,dna_sequence REGEXP '(TAA|TAG|TGA)$' as has_stop,dna_sequence like '%ATAT%' as has_atat,dna_sequence like '%ggg%' as has_ggg from samples order by sample_id;
